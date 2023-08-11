faee02e7dd0fe5eeae5dea237a0593b8a05490d2
ANKIT <itsankitagarwal000@gmail.com>
2023-08-11 21:09:12 +0530

    added overlay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{Fragment} from 'react'
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartItem from './components/Cart/CartItem';
 
 
 
 function App() {
   return (
     <Fragment>
+      <CartItem></CartItem>
       <Header></Header>
     <main>
       <Meals></Meals>
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,55 @@
+import Modal from '../UI/Modal'
+import classes from './CartItem.module.css'
+
+const CartItem = (props) =>{
+
+    const CartItem = (<ul className={classes['cart-items']}>{
+    [{id:1 , name:'sushi', amount:2, price:102}].map((item)=>(<li>{item.name}</li>))}
+    </ul>)
+    return (
+        <Modal> 
+            {CartItem}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span> 100</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+
+    )
+
+}
+
+export default CartItem
+
+// import Modal from '../UI/Modal';
+// import classes from './Cart.module.css';
+
+// const Cart = (props) => {
+//   const cartItems = (
+//     <ul className={classes['cart-items']}>
+//       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+//         <li>{item.name}</li>
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <Modal>
+//       {cartItems}
+//       <div className={classes.total}>
+//         <span>Total Amount</span>
+//         <span>35.62</span>
+//       </div>
+//       <div className={classes.actions}>
+//         <button className={classes['button--alt']}>Close</button>
+//         <button className={classes.button}>Order</button>
+//       </div>
+//     </Modal>
+//   );
+// };
+
+// export default Cart;
